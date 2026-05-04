265a90df3a21bb25174fb4535c516592c9265efd
JoriJoestar7 <santamaria.ariel.dumas@gmail.com>
2026-05-04 15:14:04 -0500

    Commit inicial

---
+++ script.py
@@ -0,0 +1,12 @@
+#------------------------------------------------------
+#Script de python para probar Git
+# Autor: Ariel Santamaria <asantamariaa1@est.ups.edu.ec>
+# Fecha: 4/05/2026
+#------------------------------------------------------
+
+def main ():
+    nombre = input("Como te llamas? ")
+    print("Hola", nombre, "bienvenido a mi mundo mano")
+
+if __name__ == "__main__":
+    main()
